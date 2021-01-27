914b78be10cf769f53908aea8af8ad0f6828433e
Jacqueline Lo <lojacqueline2@gmail.com>
2021-01-27 00:18:55 -0500

    save progress

--- src/problems.js
+++ src/problems.js
@@ -1,4 +1,4 @@
-// Frequency Counters 
+// TITLE: Frequency Counters 
 
 // example problem Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
 
@@ -48,6 +48,7 @@ function same(arr1, arr2){
     return true
 }
 
+// TITLE: Anagram - Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
 
 
 function validAnagram(first, second) {
@@ -77,4 +78,44 @@ function validAnagram(first, second) {
     return true;
   }
   
+
+//   TITLE: Multiple Pointers - Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+// 
+
+// Naive solution 
+    // Time Complexity - O(N^2)
+    // Space Complexity - O(1)
+
+
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+// Refactored 
+    // Time Complexity - O(N)
+    // Space Complexity - O(1)
+
+function sumZero(arr){
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let sum = arr[left] + arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        } else if(sum > 0){
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
+// TITLE: Sliding Window 
+
 
